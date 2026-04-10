d7c3c23c0ad8f23bca78cdb36cf7c715f602ca53
Provasish Nandi <82043200+provasish@users.noreply.github.com>
2026-04-11 00:33:43 +0530

    Update kernel repository URL in clover.sh

--- clover.sh
+++ clover.sh
@@ -23,7 +23,7 @@ git clone https://github.com/clover-spartan/android_device_realme_spartan device
 git clone https://github.com/clover-spartan/android_device_realme_sm8250-common device/realme/sm8250-common
 git clone https://github.com/clover-spartan/proprietary_vendor_realme_spartan vendor/realme/spartan
 git clone --depth=1 https://github.com/clover-spartan/proprietary_vendor_realme_sm8250-common vendor/realme/sm8250-common
-git clone --depth=1 https://github.com/clover-spartan/android_kernel_realme_sm8250 kernel/realme/sm8250
+git clone --depth=1 https://github.com/EvoX-Spartan/android_kernel_realme_sm8250 kernel/realme/sm8250
 git clone https://github.com/clover-spartan/android_hardware_dolby hardware/dolby
 git clone https://github.com/clover-spartan/android_hardware_oplus hardware/oplus
 git clone --depth=1 https://gitlab.com/provasishh/proprietary_vendor_oplus_camera.git vendor/oplus/camera
